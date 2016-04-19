4edf082d1bce44f3e59dd8c758ae0b9ca8a0bb0b
Alex Zhidkov <AlexZhidkov@users.noreply.github.com>
2016-04-19 14:33:08 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,9 @@ set -ev
 if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
   echo "about to firebase deploy dev"
   firebase deploy --token $FIREBASE_TOKEN_DEV
-  echo "after firebase deploy dev"
 fi
 
 if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     echo "about to firebase deploy"
     firebase deploy --token $FIREBASE_TOKEN
-    echo "after firebase deploy"
 fi
